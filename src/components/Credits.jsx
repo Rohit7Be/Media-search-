f8bb655e59319e59091539351e936ad39cbae4e0
Rohit singh <singhrohit77443@gmail.com>
2026-09-22 22:30:25 +0530

    add

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 const Credits = () => {
   return (
     <footer className="w-full border-t border-white/10 bg-gray-900 px-4 py-3 text-center flex items-center justify-center gap-2">
-          <p className="text-xs text-white/50">&copy; By Rohit Singh (Rohit7Be)</p>
-          <a href="http://github.com/Rohit7Be" className='text-l ' target="_blank" rel="noopener noreferrer">👉 Github</a>
+      <p className="text-xs text-white/50">
+        &copy; By Rohit Singh 👉{" "}
+      </p>
+      <a
+        href="http://github.com/Rohit7Be"
+        className="text-sm px-2 py-1 bg-black rounded-2xl opacity-60 hover:opacity-100"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
     </footer>
   );
 }
